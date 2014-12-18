13249442801f8079f24fd817391b0366474a95c2
André Roberge <andre.roberge@gmail.com>
2014-12-18 18:13:23 -0400

    just adding back user info to init

--- easygui_qt/__init__.py
+++ easygui_qt/__init__.py
@@ -1,3 +1,3 @@
-# __author__ = 'André Roberge'
-# __email__ = 'andre.roberge@gmail.com'
-# __version__ = '0.1.0'
+__author__ = 'André Roberge'
+__email__ = 'andre.roberge@gmail.com'
+__version__ = '0.1.0'
