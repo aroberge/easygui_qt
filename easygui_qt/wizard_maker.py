ba0f32a9ef1293d5a65a8bf70020ad58a6b040dd
André Roberge <andre.roberge@gmail.com>
2015-01-02 09:39:52 -0400

    starting work on wizard maker

---
+++ easygui_qt/wizard_maker.py
@@ -0,0 +1,34 @@
+
+from PyQt4 import QtGui, QtCore
+
+class WizardCreator(QtGui.QWizard):
+    def __init__(self, title="Title", pages=[]):
+        super(WizardCreator, self).__init__(None,
+              QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint)
+        self.setWindowTitle(title)
+        for page in pages:
+            self.create_page(page)
+        self.show()
+
+    def create_page(self, page):
+        new_page = QtGui.QWizardPage()
+        layout = QtGui.QVBoxLayout()
+        for kind, value in page:
+            if kind.lower() == "title":
+                new_page.setTitle(value)
+            elif kind.lower() == "text":
+                label = QtGui.QLabel(value)
+                label.setWordWrap(True)
+                layout.addWidget(label)
+        new_page.setLayout(layout)
+        self.addPage(new_page)
+
+if __name__ == '__main__':
+    app = QtGui.QApplication([])
+    page1 = [("text", "This is a sample text"), ("text", "More text")]
+    page2 = [("text", "This is another sample text"),
+             ("title", "This page has its own (sub) title")]
+    pages = [page1, page2]
+    wizard = WizardCreator(title="Hello World", pages=pages)
+    wizard.exec_()
+    app.quit()
